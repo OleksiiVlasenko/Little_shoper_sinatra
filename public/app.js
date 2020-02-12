bd26f1d17d8a86345cc0a567e1361a47c3e1ad50
FALLENRABBIT <a.a.a.vlasenko@gmail.com>
2020-02-12 22:25:33 +0200

    total order count js func + card order count

--- public/app.js
+++ public/app.js
@@ -13,8 +13,25 @@
           window.localStorage.setItem(key,x);
      
                alert('You added pizza with id: '+ x );
+               total_order_count();
      }
      function buy(){
          window.localStorage.clear();
          
+     }
+
+     function total_order_count(){
+         total =0;
+         for(var i=0;i<localStorage.length; i++)
+         {
+          var key = window.localStorage.key(i); // get key from hash Localstorage
+          var value = window.localStorage.getItem(key);
+          if (key.indexOf('product_') == 0)
+               {
+                   
+                    total +=value *1;
+               }
+         }
+       var total_out = document.getElementById('total_order_count');
+       total_out.value = total; 
      }
