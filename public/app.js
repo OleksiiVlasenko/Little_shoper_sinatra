15c0dcd8be88b841cccd88a590141defcded5b40
FALLENRABBIT <a.a.a.vlasenko@gmail.com>
2020-02-12 23:19:04 +0200

    update func for shopping cart

--- public/app.js
+++ public/app.js
@@ -13,12 +13,12 @@
           window.localStorage.setItem(key,x);
      
                // alert('You added pizza with id: '+ x );
-               total_order_count();
+               update_orders();
      }
-     function buy(){
-         window.localStorage.clear();
+     // function buy(){
+     //     window.localStorage.clear();
          
-     }
+     // }
 
      function total_order_count(){
          total =0;
@@ -32,6 +32,32 @@
                     total +=value *1;
                }
          }
-       var total_out = document.getElementById('total_order_count');
-       total_out.value = 'В корзине : '+total+' пицц'; 
+      return total;
+     }
+
+
+     function total_order_cart(){
+         orders ='';
+         for(var i=0;i<localStorage.length; i++)
+         {
+          var key = window.localStorage.key(i); // get key from hash Localstorage
+          var value = window.localStorage.getItem(key);
+          if (key.indexOf('product_') == 0)
+               {
+                   
+                   orders = orders+key+'='+value+','
+               }
+         }
+       return orders;
+
+       // var total_out = document.getElementById('total_order_count');
+       // total_out.value = orders;
+     }
+
+     function update_orders(){
+          var orders = total_order_cart();
+          var total_count_value = total_order_count();
+           $('#total_order_count').val('В корзине : '+total_count_value+' пицц'); 
+           $('#orders').val(orders); 
+           
      }
