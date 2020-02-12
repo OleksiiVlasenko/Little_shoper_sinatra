dbd0a2229653211673f8242fc23119e10dee7a22
FALLENRABBIT <a.a.a.vlasenko@gmail.com>
2020-02-12 10:44:10 +0200

    add migration products

---
+++ db/migrate/20200212082638_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+  	create_table :products do |t|
+  		t.string :title
+  		t.decimal :price
+  		t.decimal :size
+  		t.text :description
+  		t.boolean :is_spicy
+  		t.boolean :is_veg
+  		t.boolean :is_best_offer
+  		t.string :picture
+  		t.timestamps
+  	end
+  end
+end
