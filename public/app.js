226967133fea2673b8d420b3b4a3d763627f8061
FALLENRABBIT <a.a.a.vlasenko@gmail.com>
2020-02-12 17:08:35 +0200

    add id to add_cart function

--- public/app.js
+++ public/app.js
@@ -1,11 +1,15 @@
      function something()
      {
 		
-		var x = window.localStorage.getItem('aa');
-     	x = x * 1 + 1;
-     	
-     	window.localStorage.setItem('aa',x);
-     
-     		alert(x);
+		
      	
      };
+
+     function add_to_cart(id){
+         var x = window.localStorage.getItem('cart');
+          x = x * 1 + 1;
+          
+          window.localStorage.setItem('cart',x);
+     
+               alert('You added pizza with id:'+id);
+     }
