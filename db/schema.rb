2933bad3905cd23573256ae2d7a8b556c444766e
FALLENRABBIT <a.a.a.vlasenko@gmail.com>
2020-02-13 14:17:24 +0200

    add schema migration orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_084917) do
+ActiveRecord::Schema.define(version: 2020_02_13_105220) do
+
+  create_table "orders", force: :cascade do |t|
+    t.string "order_number"
+    t.decimal "quantity"
+    t.decimal "id_pizza"
+    t.decimal "total_summ"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
